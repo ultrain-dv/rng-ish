d83f8251cbd39ef87a346a523353decabeb67840
Tahjae <tahjaedev@proton.me>
2024-11-12 15:23:13 +0000

    init + Rolling function

--- rng.py
+++ rng.py
@@ -1,1 +1,23 @@
+import random
+import os
 
+while True:
+    rng = random.randint(1, 10)
+
+    os.system("clear")
+    
+    print("1. Roll \n2. Exit")
+    
+    rinput = input("[?]: ")
+    if rinput == "1":
+        print("Rolling...")
+        os.system("clear")
+        time.sleep(1)
+        print(rng)
+        os.system('pause')
+
+        
+    elif rinput == "2":
+        quit() 
+    else:
+        os.system("clear")
