3b68228c3d41d5bbd28a7c3fc9fcaddc0ef5028d
Ultrain <tahjaedev@proton.me>
2024-11-25 15:57:37 +0000

    Updated code

--- rng.py
+++ rng.py
@@ -2,24 +2,28 @@ import random
 import os
 import time
 
+
 while True:
     rng = random.randint(1, 999999999999)
 
     999999999999
     os.system("clear")
     
-    print("1. Roll \n2. Exit")
+    print("1. Roll \n2. Exit \nOr press Enter to Roll")
     
     rinput = input("[?]: ")
     if rinput == "1":
-        print("Rolling...")
-        time.sleep(1)
         os.system("clear")
         print(rng)
         time.sleep(1)
 
-        
+    elif rinput == "":
+        os.system("clear")
+        print(rng)
+        time.sleep(1)
+
     elif rinput == "2":
-        quit() 
+        quit()
+
     else:
         os.system("clear")
